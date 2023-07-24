8028002191adc5f6d9cf64d63c1447b86efc7b5a
Arceus-Tech <thawshi.s@arceus.tech>
2023-07-24 19:24:24 +0530

    added fraknie

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 
 var transport = {
@@ -20,7 +23,26 @@ transporter.verify((error, success) => {
     console.log('Server is ready to take messages');
   }
 });
-router.post('/send', (req, res, next) => {
+
+var transport_2 = {
+  host: process.env.MAIL_SERVER_2, // Don’t forget to replace with the SMTP host of your provider
+  port: process.env.MAIL_PORT_2,
+  auth: {
+  user: process.env.USER_2,
+  pass: process.env.PASS_2
+}
+}
+var transporter_2 = nodemailer.createTransport(transport_2)
+transporter_2.verify((error, success) => {
+if (error) {
+  console.log(error);
+} else {
+  console.log('Server is ready to take messages');
+}
+});
+
+
+router.post('/capital', (req, res, next) => {
   var name = req.body.first_name + " " + req.body.last_name
   var email = req.body.email
   var phone = req.body.phone
@@ -61,6 +83,133 @@ router.post('/send', (req, res, next) => {
     }
   })
 })
+
+
+
+router.post('/sap', (req, res, next) => {
+  const name = req.body.first_name + " " + req.body.last_name;
+  const email = req.body.email;
+  const company = req.body.company;
+  const website = req.body.website;
+  const details = req.body.details;
+
+  const content = `<h1>Name: ${name}</h1> \n Email: ${email} \n Company: ${company} \n Website: ${website} \n Details: ${details}`;
+
+  const mail = {
+    from: email,
+    to: 'info@securecapitaltrades.com',  // Change to the email address you want to receive messages on
+    subject: `New Message from ${email}`,
+    text: content,
+    html: `
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Nice Looking Email Template</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            padding: 20px;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #ffffff;
+            padding: 30px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #FF8C00; /* Orange color for the heading */
+            margin-bottom: 20px;
+        }
+
+        p {
+            margin-bottom: 10px;
+        }
+
+        .details-table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+            background-color: #ffffff;
+        }
+
+        .details-table th,
+        .details-table td {
+            padding: 10px;
+            border-bottom: 1px solid #ddd;
+            text-align: left;
+        }
+
+        .details-table th {
+            color: #FF8C00; /* Orange color for the table header */
+        }
+
+        .details-table td {
+            color: #333333;
+        }
+
+        .footer {
+            margin-top: 20px;
+            text-align: center;
+            color: #999999;
+        }
+
+        .footer a {
+            color: #5D3FD3; /* Orange color for the footer link */
+            text-decoration: none;
+        }
+
+        .footer a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Message from website</h1>
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Company: ${company}</p>
+        <p>Website: ${website}</p>
+
+        <table class="details-table">
+            <tr>
+                <th>Details</th>
+            </tr>
+            <tr>
+                <td>${details}</td>
+            </tr>
+        </table>
+
+        <div class="footer">
+            <p>Powered by: <a href="https://arceus.tech">Arceus Tech</a></p>
+        </div>
+    </div>
+</body>
+</html>
+
+`
+  };
+
+  transporter_2.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail',
+        error: err // Include the error message for debugging purposes
+      });
+    } else {
+      res.json({
+        status: 'success'
+      });
+    }
+  });
+});
+
+
 const app = express()
 app.use(cors())
 app.use(express.json())
